0d0813f6a4e1f7a0b30622baaa00000185f34e7a
Md. Jobaidul Islam <jobaidul.cse.brur@gmail.com>
2023-07-25 02:39:21 +0600

    changed title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
 			<Head>
 				<title>BRUR-OJ</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link rel='icon' href='/favicon.png' />
+				{/* <link rel='icon' href='/favicon.png' /> */}
 				<meta
 					name='description'
 					content='Web application that contains leetcode problems and video solutions'
